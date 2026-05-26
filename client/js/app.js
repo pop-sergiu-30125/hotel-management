2ea8beb3bb566c3123cebe5b94fe05942aad958c
pop-sergiu-30125 <pop.mi.ionut@student.utcluj.ro>
2026-05-26 14:24:07 +0300

    using crud op using axios

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,8 @@
 const API_URL = "http://localhost:3000";
 
+console.log("app.js loaded");
+
+// CLIENT VISUALIZATION
 let clientId = localStorage.getItem("clientId");
 
 if (!clientId) {
@@ -7,112 +10,57 @@ if (!clientId) {
   localStorage.setItem("clientId", clientId);
 }
 
-fetch(API_URL + "/clients/connect", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    clientId: clientId
-  })
+axios.post(API_URL + "/clients/connect", {
+  clientId: clientId
 })
   .then(function (response) {
-    return response.json();
+    console.log("Client registered:", response.data);
   })
-  .then(function (data) {
-    console.log("Client registered:", data);
+  .catch(function (error) {
+    console.log("Could not register client:", error);
   });
-
-console.log("app.js loaded");
 
 function show(data) {
   document.getElementById("output").textContent = JSON.stringify(data, null, 2);
 }
 
+// SIGN UP
 function signup() {
   console.log("signup clicked");
 
   const username = document.getElementById("signupUsername").value;
   const password = document.getElementById("signupPassword").value;
 
-  fetch(API_URL + "/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
-    })
+  axios.post(API_URL + "/signup", {
+    username: username,
+    password: password
   })
     .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      show(data);
+      show(response.data);
     })
     .catch(function (error) {
-      console.log(error);
-      show({ error: "Could not connect to server" });
-    });
-}
-
-function updateBalance() {
-  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  const id = Number(document.getElementById("balanceUserId").value);
-  const balance = Number(document.getElementById("newBalance").value);
-
-  if (!loggedUser) {
-    show({ message: "You must login first" });
-    return;
-  }
-
-  fetch(API_URL + "/users/" + id + "/balance", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      loggedUserId: loggedUser.id,
-      balance: balance
-    })
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      if (data.user && data.user.id === loggedUser.id) {
-        localStorage.setItem("loggedUser", JSON.stringify(data.user));
+      if (error.response) {
+        show(error.response.data);
+      } else {
+        show({ error: "Could not connect to server" });
       }
-
-      show(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      show({ error: "Could not connect to server" });
     });
 }
 
+// LOGIN
 function login() {
   console.log("login clicked");
 
   const username = document.getElementById("loginUsername").value;
   const password = document.getElementById("loginPassword").value;
 
-  fetch(API_URL + "/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
-    })
+  axios.post(API_URL + "/login", {
+    username: username,
+    password: password
   })
     .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+      const data = response.data;
+
       if (data.user) {
         localStorage.setItem("loggedUser", JSON.stringify(data.user));
       }
@@ -120,93 +68,24 @@ function login() {
       show(data);
     })
     .catch(function (error) {
-      console.log(error);
-      show({ error: "Could not connect to server" });
-    });
-}
-
-function loadUsers() {
-  console.log("load users clicked");
-
-  fetch(API_URL + "/users")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      show(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      show({ error: "Could not connect to server" });
-    });
-}
-
-function login() {
-  const username = document.getElementById("loginUsername").value;
-  const password = document.getElementById("loginPassword").value;
-
-  fetch(API_URL + "/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
-    })
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      if (data.user) {
-        localStorage.setItem("loggedUser", JSON.stringify(data.user));
+      if (error.response) {
+        show(error.response.data);
+      } else {
+        show({ error: "Could not connect to server" });
       }
-
-      show(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      show({ error: "Could not connect to server" });
     });
 }
 
+// LOGOUT
 function logout() {
   localStorage.removeItem("loggedUser");
-  show({ message: "Logged out" });
+
+  show({
+    message: "Logged out"
+  });
 }
 
-function deleteUser() {
-  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
-  const id = Number(document.getElementById("deleteUserId").value);
-
-  if (!loggedUser) {
-    show({ message: "You must login first" });
-    return;
-  }
-
-  fetch(API_URL + "/users/" + id, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      loggedUserId: loggedUser.id
-    })
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      show(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      show({ error: "Could not connect to server" });
-    });
-}
-
-//check on what account am i logged in
+// CHECK LOGIN STATUS
 function checkLoginStatus() {
   console.log("checkLoginStatus clicked");
 
@@ -223,11 +102,105 @@ function checkLoginStatus() {
   show({
     loggedIn: true,
     message: "You are logged in",
-    user: {
-      id: loggedUser.id,
-      username: loggedUser.username,
-      role: loggedUser.role,
-      balance: loggedUser.balance
-    }
+    user: loggedUser
   });
+}
+
+// LOAD USERS
+function loadUsers() {
+  console.log("load users clicked");
+
+  axios.get(API_URL + "/users")
+    .then(function (response) {
+      show(response.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        show(error.response.data);
+      } else {
+        show({ error: "Could not connect to server" });
+      }
+    });
+}
+
+// UPDATE USERNAME / PASSWORD
+function updateUser() {
+  const id = Number(document.getElementById("updateUserId").value);
+  const username = document.getElementById("updateUsername").value;
+  const password = document.getElementById("updatePassword").value;
+
+  axios.put(API_URL + "/users/" + id, {
+    username: username,
+    password: password
+  })
+    .then(function (response) {
+      show(response.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        show(error.response.data);
+      } else {
+        show({ error: "Could not connect to server" });
+      }
+    });
+}
+
+// UPDATE BALANCE
+function updateBalance() {
+  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+  const id = Number(document.getElementById("balanceUserId").value);
+  const balance = Number(document.getElementById("newBalance").value);
+
+  if (!loggedUser) {
+    show({ message: "You must login first" });
+    return;
+  }
+
+  axios.put(API_URL + "/users/" + id + "/balance", {
+    loggedUserId: loggedUser.id,
+    balance: balance
+  })
+    .then(function (response) {
+      const data = response.data;
+
+      if (data.user && data.user.id === loggedUser.id) {
+        localStorage.setItem("loggedUser", JSON.stringify(data.user));
+      }
+
+      show(data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        show(error.response.data);
+      } else {
+        show({ error: "Could not connect to server" });
+      }
+    });
+}
+
+// DELETE USER
+function deleteUser() {
+  const loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+  const id = Number(document.getElementById("deleteUserId").value);
+
+  if (!loggedUser) {
+    show({ message: "You must login first" });
+    return;
+  }
+
+  axios.delete(API_URL + "/users/" + id, {
+    data: {
+      loggedUserId: loggedUser.id
+    }
+  })
+    .then(function (response) {
+      show(response.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        show(error.response.data);
+      } else {
+        show({ error: "Could not connect to server" });
+      }
+    });
 }
